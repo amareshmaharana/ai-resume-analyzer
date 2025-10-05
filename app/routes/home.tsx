5f9e527d35582f3e81332aba6e0b830d1470d693
Amaresh Maharana <amareshcoder@gmail.com>
2025-10-05 16:14:11 +0530

    sample resume showed

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeScan - AI Resume Analyzer" },
+    { name: "description", content: "A AI driven resume analyzer." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+          <div className="page-heading">
+              <h1>Track your Applications & Resume ratings</h1>
+              <h2>Review your submissions & check AI-powered feedback.</h2>
+          </div>
+      </section>
+
+      {resumes.length > 0 && (
+          <div className="resumes-section">
+              {resumes.map((resume) => (
+                  <ResumeCard key={resume.id} resume={resume} />
+              ))}
+          </div>
+      )}
+  </main>;
 }
